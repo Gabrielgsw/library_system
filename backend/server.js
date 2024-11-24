f15dfd63a038ad4e94bc922d3df283186229e907
Gabrielgsw <ggermanow279@gmail.com>
2024-11-24 12:02:19 -0300

    prisma added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,20 @@ import express from 'express'
 
 const app = express();
 
+//utilizar JSON
+app.use(express.json());
+
+const livros = [];
+
+//Post
+app.post('/books',(req,res) =>{
+    livros.push(req.body);
+    res.status(201).json(req.body)
+})
+
+//Get
 app.get('/books',(req,res) =>{
-    res.send('Teste');
+    res.status(200).json(livros);
 })
 
 app.listen(3000);
